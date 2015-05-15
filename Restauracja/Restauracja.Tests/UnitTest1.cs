f07ccc6cc27189a652e7107eac515e52688d172b
hubpiet93 <pietruczukhubert@gmail.com>
2015-05-15 17:25:48 +0200

    pierwszy commit

---
+++ Restauracja/Restauracja.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Restauracja.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
